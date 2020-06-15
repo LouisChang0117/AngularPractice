a36e3e01d6b5b9eeacdf62b368cb511cab3c9ee1
Louis Chang <louis@miniasp.com>
2020-06-15 12:05:26 +0800

    0615
    修正appcomponet

--- shareDemo/src/app/app.component.ts
+++ shareDemo/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { StarRatingComponent } from './star-rating/star-rating.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  @ViewChild(StarRatingComponent) starRating: StarRatingComponent;
   title = 'shareDemo';
+  scope = '0';
+
+  showScope() {
+    this.scope = this.starRating.vcChildScope;
+  }
 }
--- shareDemo/src/app/app.module.ts
+++ shareDemo/src/app/app.module.ts
@@ -3,16 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StarRatingComponent } from './star-rating/star-rating.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, StarRatingComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
