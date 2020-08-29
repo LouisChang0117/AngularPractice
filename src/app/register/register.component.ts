37dc4b14e9d83a8ad9c5d958458e2e7a03384cd6
louischang17 <louis60406@gmail.com>
2020-08-29 16:06:22 +0800

    Add CodingStyle

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-
 import { FormBuilder, FormGroup, Validators, FormControl, FormArray } from '@angular/forms';
-
 
 @Component({
   templateUrl: './register.component.html',
@@ -9,17 +7,16 @@ import { FormBuilder, FormGroup, Validators, FormControl, FormArray } from '@ang
 })
 export class RegisterComponent implements OnInit, OnDestroy {
   data: any = {
-
     firstName: 'Will',
     lastName: 'Huang',
     email: 'doggy.huang@gmail.com',
     password: '',
-    repeatPassword: ''
+    repeatPassword: '',
   };
 
   form: FormGroup;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
     document.body.className = 'bg-gradient-primary';
@@ -35,27 +32,26 @@ export class RegisterComponent implements OnInit, OnDestroy {
     this.form = this.fb.group({
       firstName: this.fb.control('Will', {
         validators: [Validators.required],
-        updateOn: 'blur'
+        updateOn: 'blur',
       }),
       lastName: this.fb.control('Huang', {
-        validators: [Validators.required, Validators.email]
+        validators: [Validators.required, Validators.email],
       }),
       emails: this.fb.array([
         this.fb.control('doggy.huang@gmail.com', {
-          validators: [Validators.required]
+          validators: [Validators.required],
         }),
         this.fb.control('abc@example.com', {
-          validators: [Validators.required]
+          validators: [Validators.required],
         }),
       ]),
       password: this.fb.control('', {
-        validators: [Validators.required, Validators.minLength(6)]
+        validators: [Validators.required, Validators.minLength(6)],
       }),
       repeatPassword: this.fb.control('', {
-        validators: [Validators.required, Validators.minLength(6)]
-      })
+        validators: [Validators.required, Validators.minLength(6)],
+      }),
     });
-
   }
 
   ngOnDestroy(): void {
@@ -82,8 +78,7 @@ export class RegisterComponent implements OnInit, OnDestroy {
   }
 
   isInvalid(name: string): boolean {
-    return (this.fc(name).touched || this.fc(name).dirty)
-      && this.fc(name).invalid;
+    return (this.fc(name).touched || this.fc(name).dirty) && this.fc(name).invalid;
   }
 
   fc(name: string): FormControl {
@@ -96,9 +91,10 @@ export class RegisterComponent implements OnInit, OnDestroy {
 
   addNewEmail(): void {
     const emails = this.fa('emails');
-    emails.push(this.fb.control('', {
-      validators: [Validators.required, Validators.email]
-    }));
+    emails.push(
+      this.fb.control('', {
+        validators: [Validators.required, Validators.email],
+      })
+    );
   }
-
 }
